41e6529c6e3b66c878fce5e7a3746cd2825e3de0
kekich119 <patowach3o@gmail.com>
2025-06-30 11:03:01 +0300

    i am started make a new pet proj

---
+++ src/main/java/com/kekich/melofy/MelofyApplication.java
@@ -0,0 +1,13 @@
+package com.kekich.melofy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelofyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelofyApplication.class, args);
+	}
+
+}
